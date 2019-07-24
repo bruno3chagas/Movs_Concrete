b20c5eab87732be2284de1b99d776151a3074fab
Bruno Chagas <bruno3chagas@gmail.com>
2019-07-24 13:33:42 -0300

    Initial Commit

---
+++ ConcreteChallenge_BrunoChagas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcreteChallenge_BrunoChagas
+//
+//  Created by Bruno Chagas on 24/07/19.
+//  Copyright © 2019 Bruno Chagas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
